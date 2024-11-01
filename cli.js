7ba8476214430645dc95cc57fff0dc77fd5d47f4
SHASHI KUMAR KASTURI <59004150+kshashikumar@users.noreply.github.com>
2024-10-31 23:21:18 -0400

    bug fix

--- cli.js
+++ cli.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 const path = require("path");
 const nodemon = require("nodemon");
+const argv = require("minimist")(process.argv.slice(2));
+
+if (!argv.u) {
+  console.error("Error: Please provide a MySQL URL with -u option.");
+  process.exit(1);
+}
+
+process.env.MYSQL_URL = argv.u;
+process.env.PORT = argv.p || 5000;
+
 const scriptPath = path.resolve(__dirname, "src/index.js");
 
 nodemon({ script: scriptPath });
